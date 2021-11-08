92828b6addb1afce26356de958aa9edef486fcaf
VangThao1905 <vangthao1905@gmail.com>
2021-11-08 15:17:08 +0700

    create project

---
+++ counterapp/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        color: Colors.white,
+        title: 'Flutter Demo',
+        home: Scaffold(
+          body: Center(child: Text('Counter app')),
+        ));
+  }
+}
